beb8e34f487de1598966bd4ecd4d15bd62c5ffbb
Юрий <frunzeun@gmail.com>
2021-02-21 20:25:16 +0300

    HW lesson1

---
+++ script.js
@@ -0,0 +1,10 @@
+let a = "Попробуй";
+document.write("Проба")
+
+let tempC = +prompt("Введи температуру в градусах цельсия.");
+let tempF = (9 / 5) * tempC + 32;
+alert("Температура в фаренгейтах " + tempF.toFixed(1));
+
+let name = prompt("Введите имя пользователя");
+let admin = name;
+alert("Теперь админит " + name);
